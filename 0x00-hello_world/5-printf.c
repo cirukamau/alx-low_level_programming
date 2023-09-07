20d2f556c86d5ada3e047dc698387e89886c5f24
Patricia Wanjiru <patriciawanjirumd@gmail.com>
2023-09-07 11:53:45 -0700

    Hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Example of how to use printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\*n");
+return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+Hello world files for project 0x00.
